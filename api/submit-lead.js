198a173a41af4d4fa6e72b1cbeff2a0fe837b96d
Jose Sanchez <jmsan1992@gmail.com>
2026-06-09 11:01:37 +0200

    Debug: check LOOPS_API_KEY env var

--- api/submit-lead.js
+++ api/submit-lead.js
@@ -85,5 +85,5 @@ export default async function handler(req, res) {
     ]);
   }
 
-  return res.status(200).json({ ok: true });
+  return res.status(200).json({ ok: true, loopsKeySet: !!LOOPS_API_KEY });
 }
